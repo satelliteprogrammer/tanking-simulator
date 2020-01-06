16d8dc88d64759a2f6ebb4b607b62791d7368953
Satellite Programmer <satelliteprogrammer@protonmail.com>
2020-01-06 09:31:58 +0000

    multiple implementations for better mp and stats
    TimedEvent now a class
    TankHP hp now a series of tuples with time and current hp
    Adds Statistics class to save each run values
    Adds multiprocessing through pool.apply function
    Fixes function instance check at encounter logic
    Adds plots stub
    
    TODO deal with critical events

--- main.py
+++ main.py
@@ -1,42 +1,106 @@
 from collections import deque
 import buffs as b
 import encounter
+import matplotlib.pyplot as plt
+import multiprocessing as mp
+import statistics as stat
 import talents as t
+import time
 import units
 
 
 def run(fight: encounter.Fight):
-    fight.initialize(events)
+# def run(boss1, tank1, heal1, duration):
+#     fight = encounter.Fight(boss1, tank1, heal1, duration, deque())
+    fight.initialize()
     while len(fight.events) > 0:
+        # print(fight.events)
         fight.next()
 
-
-'''
-Tank: sta, agi, str, def, ddg, par, blo, b_v, armor, hit, exp, spd, talents, buffs
-'''
-tank1 = units.PaladinTank(815,  12,   0, 324, 221,  64,   0, 300, 15000,   0,   0, 1.6,
-                          (t.CombatExpertise(5), t.SacredDuty(2), t.Deflection(5), t.RighteousFury(3),
-                           t.PaladinShieldSpecialization(3), t.Anticipation(5), t.Toughness(5)),
-                          (b.Fortitude, b.MotW, b.BoK))
-
-'''
-Boss: boss, dmg min - max, spd, school, abilities
-'''
-boss1 = units.Boss('Brutallus', 73, [11521, 22721], 1.0, 'physical')
-
-'''
-Healer:                     bh,   haste, crit
-'''
-heal1 = units.PaladinHealer(1400, 0,     200)
+    return fight.finish()
 
 
-# brutallus = encounter.Fight(boss1, tank1, heal1, 480)
+if __name__ == '__main__':
 
-R = 1000
-r = 0
+    '''
+    Tank: sta, agi, str, def, ddg, par, blo, b_v, armor, hit, exp, spd, talents, buffs
+    '''
+    tank1 = units.PaladinTank(1200,  12,   0, 324, 221,  64, 100, 300, 15000,   0,   0, 1.6,
+                              (t.CombatExpertise(5), t.SacredDuty(2), t.Deflection(5), t.RighteousFury(3),
+                               t.PaladinShieldSpecialization(3), t.Anticipation(5), t.Toughness(5)),
+                              (b.Fortitude, b.MotW, b.BoK))
+    print(tank1)
 
-while r < R:
-    events = deque()
-    with encounter.Fight(boss1, tank1, heal1, 480, events) as f:
-        run(f)
-    r += 1
+    '''
+    Boss: boss, level, dmg min - max, spd, school, abilities [11521, 22721]
+    '''
+    boss1 = units.Boss('Brutallus', 73, [8000, 16000], 2.0, 'physical')
+
+    '''
+    Healer:                     bh,   haste, crit
+    '''
+    heal1 = units.PaladinHealer(2000, 100, 200)
+
+    R = 1000
+    r = 0
+
+    start = time.time()
+
+    results = list()
+    while r < R:
+        events = deque()
+        result = run(encounter.Fight(boss1, tank1, heal1, 480, events))
+        # print(result)
+        results.append(result)
+
+        r += 1
+
+
+    # pool = mp.Pool(mp.cpu_count())
+    #
+    # results = [pool.apply(run, args=(boss1, tank1, heal1, 480)) for i in range(0, R)]
+    # # results = pool.map(run, [encounter.Fight(boss1, tank1, heal1, 480, deque()) for i in range(0, R)])
+    #
+    # pool.close()
+
+    print('Elapsed time {}s'.format(time.time() - start))
+
+    deaths = []
+    missed, dodged, parried, blocked, crushed, hit, totals, critical_events = ([] for i in range(8))
+    for i, r in enumerate(results):
+        if r[7] < 480:
+            # print('Fight lasted {}s'.format(r[7]))
+            # deaths.append(i)
+            continue
+        total = r[0] + r[1] + r[2] + r[3] + r[4] + r[5]
+        totals.append(total)
+        missed.append(r[0]/total*100)
+        dodged.append(r[1]/total*100)
+        parried.append(r[2]/total*100)
+        blocked.append(r[3]/total*100)
+        crushed.append(r[4]/total*100)
+        hit.append(r[5]/total*100)
+        critical_events.append(r[6])
+
+    print('missed mean: {} variance: {}'.format(stat.mean(missed), stat.stdev(missed)))
+    print('dodged mean: {} variance: {}'.format(stat.mean(dodged), stat.stdev(dodged)))
+    print('parried mean: {} variance: {}'.format(stat.mean(parried), stat.stdev(parried)))
+    print('blocked mean: {} variance: {}'.format(stat.mean(blocked), stat.stdev(blocked)))
+    print('crushed mean: {} variance: {}'.format(stat.mean(crushed), stat.stdev(crushed)))
+    print('hit mean: {} variance: {}'.format(stat.mean(hit), stat.stdev(hit)))
+    print('critical_events mean: {} variance: {}'.format(stat.mean(critical_events), stat.stdev(critical_events)))
+
+    counter = 0
+
+    for r in results:
+        for t, hp in r[8].get_fight():
+            if hp == 0:
+                counter += 1
+
+    print('Survived {} out of {}!'.format(R - counter, R))
+
+    # if deaths:
+    #     time = [hp[0] for hp in results[deaths[0]][8].hp]
+    #     hp = [hp[1] for hp in results[deaths[0]][8].hp]
+    #     plt.plot(time, hp)
+    #     plt.show()
