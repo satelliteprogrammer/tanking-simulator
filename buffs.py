e8c9f67accfe406a9b2427444c1a5ff75a4cdd0d
Satellite Programmer <satelliteprogrammer@protonmail.com>
2020-04-16 13:11:18 +0100

    Adds new buffs

--- buffs.py
+++ buffs.py
@@ -19,15 +19,91 @@ class BoK(Buff):
         stats.strength *= 1.1
 
 
-class Fortitude(Buff):
+class iFortitude(Buff):
     @staticmethod
     def apply(stats: Stats):
-        stats.stamina += 100
+        stats.stamina += 102
 
 
-class MotW(Buff):
+class iMotW(Buff):
     @staticmethod
     def apply(stats: Stats):
         stats.stamina += 18.9
         stats.agility += 18.9
         stats.strength += 18.9
+        stats.armor += 459
+
+
+class iCommandingShout(Buff):
+    @staticmethod
+    def apply(stats: Stats):
+        stats.hp += 1350
+
+
+class DevotionAura(Buff):
+    @staticmethod
+    def apply(stats: Stats):
+        stats.armor += 861
+
+
+class FlaskOfFortification(Buff):
+    @staticmethod
+    def apply(stats: Stats):
+        def_def_ratio = 2.3654
+        stats.defense += 10/def_def_ratio
+        stats.hp += 1350
+
+
+class ElixirOfMajorAgility(Buff):
+    @staticmethod
+    def apply(stats: Stats):
+        stats.agility += 35
+
+
+class ElixirOfMajorDefense(Buff):
+    @staticmethod
+    def apply(stats: Stats):
+        stats.armor += 550
+
+
+class StaminaFood(Buff):
+    @staticmethod
+    def apply(stats: Stats):
+        stats.stamina += 30
+
+
+class AgilityFood(Buff):
+    @staticmethod
+    def apply(stats: Stats):
+        stats.agility += 20
+
+
+class ScrollOfProtection(Buff):
+    @staticmethod
+    def apply(stats: Stats):
+        stats.armor += 300
+
+
+class SunwellRadiance(Buff):
+    @staticmethod
+    def apply(stats: Stats):
+        stats.dodge -= 20
+        stats.miss -= 5
+
+
+class ScorpidSting(Buff):
+    @staticmethod
+    def apply(stats: Stats):
+        stats.miss += 5
+
+
+class InsectSwarm(Buff):
+    @staticmethod
+    def apply(stats: Stats):
+        stats.miss += 2
+
+
+class DualWielding(Buff):
+    @staticmethod
+    def apply(stats: Stats):
+        stats.miss += 20
