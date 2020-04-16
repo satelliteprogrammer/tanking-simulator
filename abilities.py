ed07fb4696649f7b94a92777dd8cf1289a34f3e8
Satellite Programmer <satelliteprogrammer@protonmail.com>
2020-04-16 13:11:51 +0100

    Adds new ability and adds attrs package to classes

--- abilities.py
+++ abilities.py
@@ -1,12 +1,31 @@
+from attr import attrs, attrib
+from typing import Tuple
+
+
+@attrs(slots=True)
 class Ability:
+    name = attrib(type=str)
+    damage = attrib(type=Tuple)
+    period = attrib(type=float)
+    school = attrib(type=str)
 
-    def __init__(self, name, damage, atk_speed, school):
-        self.name = name
-        self.damage = damage
-        self.atk_speed = atk_speed
-        self.school = school
-
-        self.atk_timer = atk_speed
+    def apply(self):
+        return self.damage
 
 
-meteor_slash = Ability('Meteor Slash', 20000/9, 15, 'fire')
+meteor_slash = Ability('Meteor Slash', (20000/9, 20000/9), 15, 'fire')
+
+
+@attrs(slots=True)
+class ArcaneBuffet(Ability):
+    inc_damage = attrib(default=500, type=int)
+    inc_duration = attrib(default=40, type=int)
+    stacks = attrib(default=0, type=int)
+
+    def apply(self):
+        damage = self.damage + self.inc_damage * self.stacks
+        self.stacks += 1
+        return damage
+
+
+arcane_buffet = ArcaneBuffet('Arcane Buffet', (463, 537), 8, 'arcane')
